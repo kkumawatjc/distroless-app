62badd06309459c50f503a24e67b92576810a81d
kkumawatjc <kuldeep.kumawat@jumpcloud.com>
2025-01-29 21:14:14 +0530

    graceful shutdown

--- main.go
+++ main.go
@@ -1,17 +1,52 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Request received from %s\n", r.RemoteAddr)
+	time.Sleep(5 * time.Second) // Simulate a long-running request
 	fmt.Fprintf(w, "Hello, Distroless!")
 }
 
 func main() {
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: http.DefaultServeMux,
+	}
+
 	http.HandleFunc("/", handler)
 	fmt.Println("Server is running on port 8080")
-	http.ListenAndServe(":8080", nil)
+
+	// Channel to listen for interrupt signals
+	stop := make(chan os.Signal, 2)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	// Run server in a goroutine
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Printf("ListenAndServe(): %v\n", err)
+		}
+	}()
+
+	// Block until we receive our signal
+	<-stop
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Attempt graceful shutdown
+	fmt.Println("Shutting down server...")
+	if err := server.Shutdown(ctx); err != nil {
+		fmt.Printf("Server Shutdown Failed:%+v", err)
+	}
+	fmt.Println("Server exited properly")
 }
